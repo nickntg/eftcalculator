28b6190e8ccc9bafcdd0ba13ba5848f228759047
nickntg_cp <SND\nickntg_cp>
2011-08-16 18:07:02 +0000

    Version 0.4.

--- EFTCalculator.Tests/PVVUnitTests.vb
+++ EFTCalculator.Tests/PVVUnitTests.vb
@@ -6,7 +6,7 @@ Public Class PVVUnitTests
 
     <TestMethod()>
     Public Sub TestPVVGeneration()
-        Assert.AreEqual("0360", Utility.GeneratePVV("5044070000253211", "1", "1234", New HexKey("91F8983761D9EAC4761FEC58FDF82A43")))
+        Assert.AreEqual("3306", Utility.GeneratePVV("5044070000253211", "1", "1234", New HexKey("91F8983761D9EAC4761FEC58FDF82A43")))
     End Sub
 
 End Class
